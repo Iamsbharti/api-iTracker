2e84c3d7f685f008180425f969319c191ba91255
Iamsbharti <saurabhbharti9@gmail.com>
2020-10-24 11:16:27 +0530

    healt check ping works

--- router/router.js
+++ router/router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-router.get("ping", (req, res) => {
+router.get("/ping", (req, res) => {
   console.log("Welcome to iTracker API");
   res.status(200).send("Welcome to iTracker API");
 });
